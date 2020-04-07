91d50232e1ebde62580b995b7b74c50522e5307c
Bradley Thompson <bradlet2@pdx.edu>
2020-04-07 15:42:02 -0700

    Implement some initial tests

--- src/test/kotlin/com/shipping_eligibility/ShippingEligibilityApplicationTests.kt
+++ src/test/kotlin/com/shipping_eligibility/ShippingEligibilityApplicationTests.kt
@@ -1,13 +1,36 @@
 package com.shipping_eligibility
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
 
-@SpringBootTest
+//Borrowed test code setup from https://medium.com/@crsandeep/creating-and-testing-a-kotlin-restful-web-services-using-spring-boot-1a11aeda279e
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class ShippingEligibilityApplicationTests {
+
+	@Autowired
+	lateinit var testRestTemplate: TestRestTemplate
 
 	@Test
 	fun contextLoads() {
+	}
+
+	@Test
+	fun noParamsShouldSendErrorStatus() {
+		val result = testRestTemplate.getForEntity("/checkAccess", String::class.java)
+		Assertions.assertEquals(result.statusCode, HttpStatus.BAD_REQUEST)
+	}
+	@Test
+	fun blankParamsShouldThrowException() {
+		val badRequest = "?title=''&sender=''&category=''&price='-1'"
+		val result = testRestTemplate.getForEntity("/checkAccess$badRequest", String::class.java)
+		Assertions.assertEquals(result.statusCode, HttpStatus.BAD_REQUEST)
 	}
 
 }
