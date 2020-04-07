ec8e10b1839809f153f57e362daa19ba11ed26b7
Bradley Thompson <bradlet2@pdx.edu>
2020-04-06 19:18:12 -0700

    Create 2 practice endpoints

--- src/main/kotlin/com/shipping_eligibility/ShippingEligibilityApplication.kt
+++ src/main/kotlin/com/shipping_eligibility/ShippingEligibilityApplication.kt
@@ -3,6 +3,7 @@ package com.shipping_eligibility
 import org.springframework.boot.runApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
@@ -12,6 +13,16 @@ class ShippingEligibilityApplication {
 	@GetMapping("/")
 	fun home(): String {
 		return "Hello World!"
+	}
+
+	@GetMapping("/shipping")
+	fun shipping(@RequestParam(value="title", defaultValue="none") name: String): Boolean {
+		if (name == "none") {
+			println("GET: No name supplied")
+			return false
+		}
+		println("Title supplied: $name")
+		return true
 	}
 }
 
