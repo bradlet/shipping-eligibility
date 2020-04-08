34b0d8570b39d1719e57364f64c0061617461446
Bradley Thompson <bradlet2@pdx.edu>
2020-04-07 17:31:11 -0700

    Add generalized request checks

--- src/main/kotlin/com/shipping_eligibility/EligibilityRequest.kt
+++ src/main/kotlin/com/shipping_eligibility/EligibilityRequest.kt
@@ -2,11 +2,8 @@ package com.shipping_eligibility
 
 // Holds all information associated with a request to check shipping program eligibility.
 // Also ensures valid data, and could hold data persistence functionality.
-class EligibilityRequest(
-        val title: String,
-        val seller: String,
-        val category: String,
-        val price: Double
+class EligibilityRequest(private val title: String, private val seller: String,
+                         private val category: String, private val price: Double
 ) {
     // Throws an exception if params are blank, or if a negative price was supplied.
     fun validate(){
@@ -15,5 +12,35 @@ class EligibilityRequest(
             println("throw InvalidInputException")
             throw InvalidInputException("BAD REQUEST: $title, $seller, $category, $price")
         }
+    }
+
+    // Run all checks to determine if this request is
+    // eligible to access the shipping program
+    fun checkRequest(sellerList: Array<String>, categoryList: Array<String>,
+                     minPrice: Int): Boolean {
+        if (checkSeller(sellerList) && checkCategory(categoryList) && checkPrice(minPrice))
+            return true
+        return false
+    }
+
+    // Return true if this EligibilityRequest's seller is in the list supplied
+    fun checkSeller(list: Array<String>): Boolean {
+        if (seller in list)
+            return true
+        return false
+    }
+
+    // Return true if this EligibilityRequest's category is in the list supplied
+    fun checkCategory(list: Array<String>): Boolean {
+        if (category in list)
+            return true
+        return false
+    }
+
+    // Return true if this EligibilityRequest's price is >= supplied minimum price
+    fun checkPrice(minPrice: Int): Boolean {
+        if (price >= minPrice)
+            return true
+        return false
     }
 }
